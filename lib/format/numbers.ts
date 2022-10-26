faa514e6e62c03950d34211b0c5372eb1a18962a
OLU-DEVX <shedrachjonah22@gmail.com>
2022-10-26 22:38:39 +0000

    feat(format): add formatCompactNumber for K/M/B notation

--- lib/format/numbers.ts
+++ lib/format/numbers.ts
@@ -10,3 +10,18 @@ export function formatNumber(
 ): string {
   return new Intl.NumberFormat(locale).format(value);
 }
+
+/**
+ * Format a number in compact notation, e.g. 1234 -> "1.2K", 2_400_000 -> "2.4M".
+ * Falls back to {@link formatNumber} for values < 1000.
+ */
+export function formatCompactNumber(
+  value: number,
+  locale: string = "en-US"
+): string {
+  if (Math.abs(value) < 1000) return formatNumber(value, locale);
+  return new Intl.NumberFormat(locale, {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(value);
+}
