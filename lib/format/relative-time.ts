4db7d45abe877ecbe5bbf4b98ef4ca70b24f3181
OLU-DEVX <shedrachjonah22@gmail.com>
2022-11-03 08:32:33 +0000

    feat(format): add formatRelativeTime using Intl.RelativeTimeFormat

---
+++ lib/format/relative-time.ts
@@ -0,0 +1,33 @@
+/**
+ * Format the distance between two timestamps as a human-readable
+ * string, e.g. "2 minutes ago", "in 3 hours".
+ *
+ * Uses Intl.RelativeTimeFormat under the hood.
+ */
+const DIVISIONS: Array<{ amount: number; name: Intl.RelativeTimeFormatUnit }> = [
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+];
+
+export function formatRelativeTime(
+  date: Date | string | number,
+  now: Date = new Date(),
+  locale: string = "en-US"
+): string {
+  const target = date instanceof Date ? date : new Date(date);
+  const formatter = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
+
+  let duration = (target.getTime() - now.getTime()) / 1000;
+  for (const division of DIVISIONS) {
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name);
+    }
+    duration /= division.amount;
+  }
+  return formatter.format(Math.round(duration), "years");
+}
