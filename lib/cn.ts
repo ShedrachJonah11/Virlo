4491abcfa545b87d9591bb4ced52b6cdd1517690
OLU-DEVX <shedrachjonah22@gmail.com>
2023-01-21 10:07:29 +0000

    feat(lib): add lib/cn.ts re-export of cn

---
+++ lib/cn.ts
@@ -0,0 +1,7 @@
+/**
+ * Re-export of `cn` — the project's class-name combiner.
+ * `import { cn } from "@/lib/cn"` reads slightly nicer in JSX
+ * than `from "@/lib/utils"` and survives any future refactors
+ * of `lib/utils.ts`.
+ */
+export { cn } from "./utils";
