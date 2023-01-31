d6a55ab98f3fe5bb1b8527afe2f566df443b25fb
OLU-DEVX <shedrachjonah22@gmail.com>
2023-01-31 07:19:21 +0000

    refactor(utils): document cn() responsibilities

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Compose Tailwind class names with conflict resolution.
+ * Re-exported from `lib/cn.ts` and `lib/cx.ts` for consistency
+ * with the rest of the project.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
