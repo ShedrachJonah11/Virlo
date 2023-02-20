2bcbdbce54e6ee71cc71d1aeacbf884f2aa82c99
OLU-DEVX <shedrachjonah22@gmail.com>
2023-02-20 01:43:05 +0000

    feat(lib): add uuid() with crypto.randomUUID fallback

---
+++ lib/uuid.ts
@@ -0,0 +1,22 @@
+/**
+ * Generate a v4-style UUID. Uses `crypto.randomUUID` where available
+ * and falls back to a deterministic-format string built from
+ * `crypto.getRandomValues` for older runtimes.
+ */
+export function uuid(): string {
+  if (typeof crypto !== "undefined" && "randomUUID" in crypto) {
+    return crypto.randomUUID();
+  }
+  const bytes = new Uint8Array(16);
+  if (typeof crypto !== "undefined" && crypto.getRandomValues) {
+    crypto.getRandomValues(bytes);
+  } else {
+    for (let i = 0; i < bytes.length; i += 1) bytes[i] = Math.floor(Math.random() * 256);
+  }
+  bytes[6] = (bytes[6] & 0x0f) | 0x40;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+  const hex = Array.from(bytes, (b) => b.toString(16).padStart(2, "0"));
+  return `${hex.slice(0, 4).join("")}-${hex.slice(4, 6).join("")}-${hex
+    .slice(6, 8)
+    .join("")}-${hex.slice(8, 10).join("")}-${hex.slice(10, 16).join("")}`;
+}
