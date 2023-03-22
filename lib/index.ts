cfecb5a7e8192fc0035a67176a0ffc91a6a4fefc
OLU-DEVX <shedrachjonah22@gmail.com>
2023-03-22 22:57:40 +0000

    feat(lib): add slim top-level barrel for cn + constants + initials

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Top-level re-exports for the most common helpers.
+ * Deep imports (e.g. `@/lib/format/numbers`) still work — this is
+ * a convenience surface only.
+ */
+export { cn } from "./utils";
+export * from "./initials";
+export * from "./constants";
