710210a185e7bcd39fc7669665cc77a5e8b9822d
OLU-DEVX <shedrachjonah22@gmail.com>
2023-03-26 15:54:37 +0000

    fix(lib): resolve uuid() crypto narrowing under strict mode

--- lib/uuid.ts
+++ lib/uuid.ts
@@ -4,14 +4,22 @@
  * `crypto.getRandomValues` for older runtimes.
  */
 export function uuid(): string {
-  if (typeof crypto !== "undefined" && "randomUUID" in crypto) {
-    return crypto.randomUUID();
+  const c: Crypto | undefined =
+    typeof globalThis !== "undefined" && "crypto" in globalThis
+      ? (globalThis as unknown as { crypto: Crypto }).crypto
+      : undefined;
+
+  if (c && typeof c.randomUUID === "function") {
+    return c.randomUUID();
   }
+
   const bytes = new Uint8Array(16);
-  if (typeof crypto !== "undefined" && crypto.getRandomValues) {
-    crypto.getRandomValues(bytes);
+  if (c && typeof c.getRandomValues === "function") {
+    c.getRandomValues(bytes);
   } else {
-    for (let i = 0; i < bytes.length; i += 1) bytes[i] = Math.floor(Math.random() * 256);
+    for (let i = 0; i < bytes.length; i += 1) {
+      bytes[i] = Math.floor(Math.random() * 256);
+    }
   }
   bytes[6] = (bytes[6] & 0x0f) | 0x40;
   bytes[8] = (bytes[8] & 0x3f) | 0x80;
