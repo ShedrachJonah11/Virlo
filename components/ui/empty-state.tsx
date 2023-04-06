3f30d71d2445263e20f90071d8af41fccdde053d
OLU-DEVX <shedrachjonah22@gmail.com>
2023-04-06 18:45:28 +0000

    feat(ui): add EmptyState reusable panel

---
+++ components/ui/empty-state.tsx
@@ -0,0 +1,40 @@
+import type { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface EmptyStateProps {
+  icon?: LucideIcon;
+  title: string;
+  description?: string;
+  action?: React.ReactNode;
+  className?: string;
+}
+
+/**
+ * Standard empty-state panel. Centred icon + title + optional action.
+ * Use anywhere a list/table/grid renders zero rows.
+ */
+export function EmptyState({
+  icon: Icon,
+  title,
+  description,
+  action,
+  className,
+}: EmptyStateProps) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center gap-3 rounded-lg border border-dashed bg-card p-10 text-center",
+        className
+      )}
+    >
+      {Icon ? <Icon className="size-8 text-muted-foreground" aria-hidden /> : null}
+      <div className="space-y-1">
+        <h3 className="text-base font-semibold">{title}</h3>
+        {description ? (
+          <p className="max-w-md text-sm text-muted-foreground">{description}</p>
+        ) : null}
+      </div>
+      {action}
+    </div>
+  );
+}
