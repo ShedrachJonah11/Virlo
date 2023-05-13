6aec131fa48a134f8ef6b61da33cee141d40ece3
OLU-DEVX <shedrachjonah22@gmail.com>
2023-05-13 20:14:58 +0000

    refactor(format): formatBytes now supports decimal (SI) and binary (IEC) scales

--- lib/format/bytes.ts
+++ lib/format/bytes.ts
@@ -1,21 +1,31 @@
-const UNITS = ["B", "KB", "MB", "GB", "TB", "PB"] as const;
+const UNITS_BINARY = ["B", "KiB", "MiB", "GiB", "TiB", "PiB"] as const;
+const UNITS_DECIMAL = ["B", "KB", "MB", "GB", "TB", "PB"] as const;
+
+export interface FormatBytesOptions {
+  /** Number of fraction digits. Defaults to 1. */
+  digits?: number;
+  /** Use 1024-based (binary) or 1000-based (SI) scale. Defaults to "decimal". */
+  unit?: "binary" | "decimal";
+}
 
 /**
- * Format a byte count using the binary IEC scale (1024-based).
- *
- *   1023 -> "1023 B"
- *   1024 -> "1.0 KB"
- *   1_500_000 -> "1.4 MB"
+ * Format a byte count. Defaults to the SI (1000-based) scale which is
+ * what most users expect when seeing "MB" / "GB" — the binary scale
+ * uses the IEC suffixes ("MiB", "GiB").
  */
-export function formatBytes(bytes: number, digits: number = 1): string {
+export function formatBytes(bytes: number, options: FormatBytesOptions = {}): string {
+  const { digits = 1, unit = "decimal" } = options;
   if (!Number.isFinite(bytes) || bytes < 0) return "0 B";
-  if (bytes < 1024) return `${bytes} B`;
+  const base = unit === "binary" ? 1024 : 1000;
+  const units = unit === "binary" ? UNITS_BINARY : UNITS_DECIMAL;
+
+  if (bytes < base) return `${bytes} B`;
 
   let i = 0;
   let value = bytes;
-  while (value >= 1024 && i < UNITS.length - 1) {
-    value /= 1024;
+  while (value >= base && i < units.length - 1) {
+    value /= base;
     i += 1;
   }
-  return `${value.toFixed(digits)} ${UNITS[i]}`;
+  return `${value.toFixed(digits)} ${units[i]}`;
 }
