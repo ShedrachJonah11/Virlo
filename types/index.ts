c2e82298865010ab21e43c86d9ebf85414138a03
OLU-DEVX <shedrachjonah22@gmail.com>
2023-05-20 00:29:53 +0000

    refactor(types): extract InvoiceStatus literal

--- types/index.ts
+++ types/index.ts
@@ -48,11 +48,13 @@ export interface AnalyticsData {
   algorithmScore: number;
 }
 
+export type InvoiceStatus = "paid" | "pending" | "failed";
+
 export interface Invoice {
   id: string;
   date: string;
   amount: number;
-  status: "paid" | "pending" | "failed";
+  status: InvoiceStatus;
   description: string;
 }
 
