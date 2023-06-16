65782ae12d5394cede2827f373db6833cacf0844
OLU-DEVX <shedrachjonah22@gmail.com>
2023-06-16 04:47:31 +0000

    feat(email): add wrapHtml minimal HTML envelope

--- lib/email/templates.ts
+++ lib/email/templates.ts
@@ -26,3 +26,17 @@ ${resetUrl}
 If you didn't request this, ignore this email.`,
   };
 }
+
+/**
+ * Minimal HTML wrapper. Inlines a single brand color block so the
+ * email renders even with all styles stripped (most email clients).
+ */
+export function wrapHtml(title: string, body: string): string {
+  return `<!doctype html>
+<html lang="en"><head><meta charset="utf-8"><title>${title}</title></head>
+<body style="font-family: -apple-system, system-ui, sans-serif; color: #111; padding: 24px; max-width: 560px;">
+<h1 style="font-size: 18px; margin: 0 0 16px;">${title}</h1>
+${body}
+</body></html>`;
+}
+
