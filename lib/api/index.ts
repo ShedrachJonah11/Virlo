07a5bec7480da993cbb624ad79f14519e3952dae
OLU-DEVX <shedrachjonah22@gmail.com>
2023-06-19 21:44:28 +0000

    feat(api): add ApiOptions alias as a forward-compat surface

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -2,3 +2,4 @@ export * from "./http";
 export * from "./retry";
 export * from "./request-id";
 export * from "./with-retry";
+export * from "./types";
---
+++ lib/api/types.ts
@@ -0,0 +1,4 @@
+import type { HttpOptions } from "./http";
+
+/** Alias surface for future widening (interceptors, telemetry, ...). */
+export type ApiOptions = HttpOptions;
