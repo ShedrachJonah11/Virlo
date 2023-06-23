d02eadb4e5f26733c907ffd11c204490592af961
OLU-DEVX <shedrachjonah22@gmail.com>
2023-06-23 14:41:25 +0000

    feat(social): add videoUrl deep-link builder

--- lib/social/handles.ts
+++ lib/social/handles.ts
@@ -19,3 +19,17 @@ export function displayHandle(handle: string): string {
   const u = normaliseUsername(handle);
   return u ? `@${u}` : "";
 }
+
+/** Build a deep link to a creator's specific video. */
+export function videoUrl(platform: "tiktok" | "instagram" | "youtube", handle: string, id: string): string {
+  const u = normaliseUsername(handle);
+  switch (platform) {
+    case "tiktok":
+      return `https://www.tiktok.com/@${u}/video/${id}`;
+    case "instagram":
+      return `https://www.instagram.com/p/${id}`;
+    case "youtube":
+      return `https://www.youtube.com/watch?v=${id}`;
+  }
+}
+
