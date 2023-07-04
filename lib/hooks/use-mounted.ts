2fa883af307bf8d5612cd4d88cc88091a41617fd
OLU-DEVX <shedrachjonah22@gmail.com>
2023-07-04 17:32:16 +0000

    fix(hooks): silence intentional set-state-in-effect in useMounted

--- lib/hooks/use-mounted.ts
+++ lib/hooks/use-mounted.ts
@@ -6,10 +6,14 @@ import { useEffect, useState } from "react";
  * Returns `true` after the component has mounted on the client.
  * Useful for gating client-only UI (e.g. theme switchers) to avoid
  * hydration mismatches.
+ *
+ * The single set-state in the effect is intentional — that's the
+ * whole point of the hook.
  */
 export function useMounted(): boolean {
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
   return mounted;
