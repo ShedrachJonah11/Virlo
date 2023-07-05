01c0d1616348cd8fafa4a3d1d95ac6b45f0a5a6e
OLU-DEVX <shedrachjonah22@gmail.com>
2023-07-05 23:11:15 +0000

    fix(hooks): seed useOnlineStatus from navigator.onLine in initial state

--- lib/hooks/use-online-status.ts
+++ lib/hooks/use-online-status.ts
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 
 /** Reports `navigator.onLine` and updates when connectivity changes. */
 export function useOnlineStatus(): boolean {
-  const [online, setOnline] = useState<boolean>(true);
+  const [online, setOnline] = useState<boolean>(() => {
+    if (typeof window === "undefined") return true;
+    return window.navigator.onLine;
+  });
 
   useEffect(() => {
     if (typeof window === "undefined") return;
-    setOnline(window.navigator.onLine);
     const onOnline = () => setOnline(true);
     const onOffline = () => setOnline(false);
     window.addEventListener("online", onOnline);
